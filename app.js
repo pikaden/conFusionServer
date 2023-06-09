ca237338af231bec1f4edabf82ba07a2ca7db17e
pikaden <87664605+pikaden@users.noreply.github.com>
2023-06-09 17:57:44 +0700

    passport authentication

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -33,27 +35,42 @@ app.use(session({
   store: new FileStore()
 }))
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 // login first
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
-    var err = new Error('You are not authenticated');
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
+    next(err);
   } else {
-    if (req.session.user === 'authenticated') {
-      next();
-    } else {
-      var err = new Error('You are not authenticated');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
+
+// function auth(req, res, next) {
+//   console.log(req.session);
+
+//   if (!req.session.user) {
+//     var err = new Error('You are not authenticated');
+//     err.status = 403;
+//     return next(err);
+//   } else {
+//     if (req.session.user === 'authenticated') {
+//       next();
+//     } else {
+//       var err = new Error('You are not authenticated');
+//       err.status = 403;
+//       return next(err);
+//     }
+//   }
+// }
 
 // function auth(req, res, next) {
 //   console.log(req.session);
